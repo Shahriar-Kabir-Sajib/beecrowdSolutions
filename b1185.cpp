01567e42a75de380284137af6a04fb1a6c769cd5
MD SHAHRIAR KABIR SAJIB <145272378+Shahriar-Kabir-Sajib@users.noreply.github.com>
2023-10-30 18:25:33 +0600

    Add files via upload

---
+++ b1185.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    // Initialization of character
+    char ch;
+    cin >> ch;
+    // Initialization of 2D Array
+    int i, j;
+    double M[12][12];
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 12; j++)
+        {
+            cin >> M[i][j];
+        }
+    }
+    // Sum and avg
+    double sum = 0, avg;
+    for (i = 0; i < 12; i++)
+    {
+        for (j = 0; j < 12; j++)
+        {
+            if (j < 12 - 1 - i)
+            {
+                sum = sum + M[i][j];
+            }
+        }
+    }
+    // Printing sum
+    if (ch == 'S')
+    {
+        cout << fixed << setprecision(1) << sum << endl;
+    }
+    // Printing avg
+    else if (ch == 'M')
+    {
+        avg = sum / (double)66;
+        cout << fixed << setprecision(1) << avg << endl;
+    }
+
+    return 0;
+}
